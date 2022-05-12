f4f11bd6148fde76b00d8f70c6cc31146f035ced
Chenwi Eugene <chenwieugene.j@gmail.com>
2022-05-12 14:44:04 +0100

    added the ability to add others to my stream

--- public/script.js
+++ public/script.js
@@ -1,8 +1,15 @@
 // to create the functionality to show our own video
-
+const socket = io("/");
 const videoGrid = document.querySelector("#video_grid");
 const myVideo = document.createElement("video");
-myVideo.muted = true;
+myVideo.muted = false;
+
+// Create a peer
+var peer = new Peer(/* the first parameter is the id, but we will let peer generate it so we will */ undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '3010'
+});
 
 // This variable will recieve the stream recieved from the navigator mediabelow
 let myVideoStream;
@@ -13,11 +20,51 @@ navigator.mediaDevices
     Audio: true,
   })
   .then((stream) => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream);
-  });
+    try {
+      myVideoStream = stream;
+      addVideoStream(myVideo, stream);
+    } catch (e) {
+      console.error(e);
+    }
+
+    peer.on('call', call => {
+      call.answer(stream)
+      const video = document.createElement('video')
+      call.on('stream', )
+    })
+
+    // Here we need to then listen to the responds of user connected and then do something
+    socket.on("user-connected", (userId) => {
+      console.log("Catching user-connected event");
+      connectToNewUser(userId, stream);
+    });
+  }).catch(e => {
+    console.log('You Have To Connect You Webcam')
+  })
+
+peer.on('open', id => {
+  console.log(id);
+  // This will then show the person who joined the room in the frontend
+socket.emit("join-room", ROOM_ID, id);
+})
+
+
+
+
+const connectToNewUser = (userId, stream) => {
+
+  // We call the user that connects like this
+  const call = peer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+};
 
 const addVideoStream = (video, stream) => {
+  if (!stream) {
+    throw 'There Is No Camera Man!'
+  }
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
     video.play();
--- server.js
+++ server.js
@@ -6,13 +6,27 @@ const app = express();
 // To create the server
 const server = require("http").Server(app);
 
+// This creates a websocket for real live comunication
+const io = require("socket.io")(server);
+
 const { v4: uuidv4 } = require("uuid");
+
+// Import peerJs and set up a serve with it for peer-to-peer connection to be able to share audio and videos etc,
+const {ExpressPeerServer} = require('peer');
+const { debug } = require("console");
+ 
+const peerServer = ExpressPeerServer(server, () => {
+  debug: true 
+})
 
 // This sets the view engine, without which the app will not be able to identify the ejs file
 app.set("view engine", "ejs");
 
 // This specifies the path for our public folder
 app.use(express.static("public"));
+
+// Base url for peerJs server
+app.use('/peerjs', peerServer)
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
@@ -21,7 +35,21 @@ app.get("/", (req, res) => {
 // ***NB: Without the colon(:) before the room after /, the url will not work.
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
+}); 
+
+io.on("connection", (socket) => {
+  // This accepts the socket.emit('join-room') call that is done in the frontend script.js
+  socket.on("join-room", (roomId, userId) => {
+    // This now does the actual joining of the new user, using the user's roomId
+    socket.join(roomId);
+
+    // This then shows the new user in the frontend
+    console.log({ roomId })
+    socket.broadcast.emit('user-connected', userId)
+  });
 });
 
 // to get the server tlistenig to activities in a port
-server.listen(3010);
+server.listen(3010, () => {
+  console.log(`Successfully Connected To Server at Port: 3010`);
+});
